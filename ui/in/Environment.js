d40e86f423fd1135f54cc877eaa970d4fe3da99b
Oliv Utilo <olivutilo@gmail.com>
2018-09-20 12:12:14 +0100

    scripts

--- ui/in/Environment.js
+++ ui/in/Environment.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter } from 'react-router-dom'
 
 const client = new ApolloClient({ uri: '/graphql' })
 
-export default class Environment extends React.Component {
-	render () {
-		return (<ApolloProvider client={client}>
-			<Router>
-				{this.props.children}
-			</Router>
-		</ApolloProvider>)
-	}
-}
+export default const Environment ({ children }) => (<ApolloProvider client={client}>
+	<BrowserRouter>
+		{children}
+	</BrowserRouter>
+</ApolloProvider>)
